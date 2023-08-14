78eee4353f85a9110adb58b7d12262e7a9aa40bc
Kundan Bhasin <iamkundanb@gmail.com>
2023-08-15 01:31:19 +0530

    just formatted response code

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        return $this->respondWith(message: '😃 Welcome to TDS', data: 'The Era of Cashbacks');
+        return $this->respondWith(
+            message: '😃 Welcome to TDS',
+            data: 'The Era of Cashbacks'
+        );
     }
 }
