c9ae512ef9a4332e00b39297c206c360e2a44a5f
Kundan Bhasin <iamkundanb@gmail.com>
2023-08-15 01:18:05 +0530

    created welcome API

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,52 +7,44 @@ use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\API\ResponseTrait;
 use Psr\Log\LoggerInterface;
 
 /**
  * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
  */
 abstract class BaseController extends Controller
 {
+    use ResponseTrait;
+
     /**
-     * Instance of the main Request object.
-     *
      * @var CLIRequest|IncomingRequest
      */
     protected $request;
 
     /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
      * @var array
      */
     protected $helpers = [];
-
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
 
     /**
      * @return void
      */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Do Not Edit This Line
         parent::initController($request, $response, $logger);
+    }
 
-        // Preload any models, libraries, etc, here.
+    protected function respondWith($success = 1, $message = null, $data = null, $status = 200)
+    {
+        $finalResponse = ['success' => $success];
 
-        // E.g.: $this->session = \Config\Services::session();
+        if ($message != null)
+            $finalResponse['message'] = $message;
+
+        if ($data != null)
+            $finalResponse['data'] = $data;
+
+        return $this->respond($finalResponse, $status);
     }
 }
